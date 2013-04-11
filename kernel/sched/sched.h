b2c30511bf630d4aa3bec003e730da2dc3713993
Frederic Weisbecker <fweisbec@gmail.com>
2013-04-12 01:51:02 +0200

    sched: Use an accessor to read the rq clock
    Read the runqueue clock through an accessor. This
    prepares for adding a debugging infrastructure to
    detect missing or redundant calls to update_rq_clock()
    between a scheduler's entry and exit point.
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Li Zhong <zhong@linux.vnet.ibm.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Paul Turner <pjt@google.com>
    Cc: Mike Galbraith <efault@gmx.de>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Conflicts:
    	kernel/sched/fair.c
    	kernel/sched/stats.h

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -706,6 +706,16 @@ static inline u64 rq_clock_task(struct rq *rq)
 DECLARE_PER_CPU(struct nr_stats_s, runqueue_stats);
 #endif
 
+static inline u64 rq_clock(struct rq *rq)
+{
+	return rq->clock;
+}
+
+static inline u64 rq_clock_task(struct rq *rq)
+{
+	return rq->clock_task;
+}
+
 #ifdef CONFIG_SMP
 
 #define rcu_dereference_check_sched_domain(p) \
