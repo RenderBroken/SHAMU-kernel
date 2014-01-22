b771f7a6e1aa871d42c952bb3d07c932b4405c40
Sai Gurrappadi <sgurrappadi@nvidia.com>
2014-01-21 16:41:37 -0800

    sched: Force sleep on consecutive sched_yields
    If a task sched_yields to itself continuously, force the task to
    sleep in sched_yield. This will lower the CPU load of this task
    thereby lowering the cpu frequency and improving power.
    
    Added a stat variable to track how many times we sleep due these
    consecutive sched_yields. Also added sysctl knobs to control the number
    of consecutive sched_yields before which the sleep kicks in and the
    duration fo the sleep in us.
    
    Bug 1424617
    
    Change-Id: Ie92412b8b900365816e17237fcbd0aac6e9c94ce
    Signed-off-by: Sai Gurrappadi <sgurrappadi@nvidia.com>
    Reviewed-on: http://git-master/r/358455
    Reviewed-by: Wen Yi <wyi@nvidia.com>
    Reviewed-by: Peter Zu <pzu@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Diwakar Tundlam <dtundlam@nvidia.com>
    Signed-off-by: Zachariah <zkennedy87@gmail.com>
    
    Conflicts:
    
    	kernel/sched/core.c
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    Conflicts:
    	kernel/sched/core.c

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -319,6 +319,19 @@ __read_mostly int scheduler_running;
 int sysctl_sched_rt_runtime = 950000;
 
 /*
+ * Number of sched_yield calls that result in a thread yielding
+ * to itself before a sleep is injected in its next sched_yield call
+ * Setting this to -1 will disable adding sleep in sched_yield
+ */
+const_debug int sysctl_sched_yield_sleep_threshold = 4;
+
+/*
+ * Sleep duration in us used when sched_yield_sleep_threshold
+ * is exceeded.
+ */
+const_debug unsigned int sysctl_sched_yield_sleep_duration = 50;
+
+/*
  * Maximum bandwidth available for all -deadline tasks and groups
  * (if group scheduling is configured) on each CPU.
  *
@@ -3499,6 +3512,7 @@ need_resched:
 	if (likely(prev != next)) {
 		rq->nr_switches++;
 		rq->curr = next;
+		prev->yield_count = 0;
 		++*switch_count;
 
 		context_switch(rq, prev, next); /* unlocks the rq */
@@ -3510,8 +3524,10 @@ need_resched:
 		 */
 		cpu = smp_processor_id();
 		rq = cpu_rq(cpu);
-	} else
+	} else {
+		prev->yield_count++;
 		raw_spin_unlock_irq(&rq->lock);
+	}
 
 	post_schedule(rq);
 
@@ -5202,6 +5218,8 @@ SYSCALL_DEFINE0(sched_yield)
 	struct rq *rq = this_rq_lock();
 
 	schedstat_inc(rq, yld_count);
+	if (rq->curr->yield_count == sysctl_sched_yield_sleep_threshold)
+		schedstat_inc(rq, yield_sleep_count);
 	current->sched_class->yield_task(rq);
 
 	/*
@@ -5213,7 +5231,11 @@ SYSCALL_DEFINE0(sched_yield)
 	do_raw_spin_unlock(&rq->lock);
 	sched_preempt_enable_no_resched();
 
-	schedule();
+	if (rq->curr->yield_count == sysctl_sched_yield_sleep_threshold)
+		usleep_range(sysctl_sched_yield_sleep_duration,
+				sysctl_sched_yield_sleep_duration + 5);
+	else
+		schedule();
 
 	return 0;
 }
