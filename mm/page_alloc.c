e5f3c552d5456f4793e3d406f40be202001d77d1
Pranav Vashi <neobuddy89@gmail.com>
2016-04-18 10:09:12 +0530

    mm: zcache: Tune and fix backport
    * Use compatible shrinker API.
    * Enable by default. If you're compiling it, you will use it.
    * Use LZ4 crypto compressor, if available.
    * Remove warning.
    * Add respective flag in page-alloc.
    * Hail Hydra.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -6367,6 +6367,9 @@ static const struct trace_print_flags pageflag_names[] = {
 	{1UL << PG_compound_lock,	"compound_lock"	},
 #endif
 	{1UL << PG_readahead,           "PG_readahead"  },
+#ifdef CONFIG_ZCACHE
+	{1UL << PG_was_active,           "PG_was_active"  },
+#endif
 };
 
 static void dump_page_flags(unsigned long flags)
